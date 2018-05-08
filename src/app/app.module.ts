348f664f6eabadb5bce9eec320ee06cc1a3aac02
rogeri10o <rogeri10o@yahoo.com.br>
2018-05-07 22:32:45 -0300

    Salvando os dados do usuario logado no localStorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { CategoriaService } from '../services/domain/categoria.service';
 import { ErrorInterceptor } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
+import { StorageService } from '../services/storage.service';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { AuthService } from '../services/auth.service';
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
     ErrorInterceptor,
-    AuthService
+    AuthService,
+    StorageService
   ]
 })
 export class AppModule {}
