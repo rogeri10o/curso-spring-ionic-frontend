2f45c77d269da728e9582e93aacd6cb1c6d7566c
rogeri10o <rogeri10o@yahoo.com.br>
2018-05-20 16:40:38 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,5 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    // baseUrl: "http://localhost:8080",
+    baseUrl: "https://curso-spring-ionic-rogerio.herokuapp.com/",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-rogerio"
 }
